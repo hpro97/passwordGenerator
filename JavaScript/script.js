0075b1f64be2e09e3bf71529ccc9d63d82877150
Hpro97 <haxmo25@gmail.com>
2024-01-06 22:42:54 +0000

    structure written, buggy code, not working as intended

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -88,23 +88,92 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
-
-}
-
 // Function for getting a random element from an array
-function getRandom(arr) {
+// function getRandom(arr) {
 
-}
+// }
 
+//maybe need adding
+
+
+//------------------------------------------------------//
+//---------------------my code--------------------------//
+//------------------------------------------------------//
+
+// * Generate a password when the button is clicked
 // Function to generate password with user input
+
+//------------------------------------------------------//
+//---------link to button when clicked in html----------//
+//------------------------------------------------------//
+
+
+//   * Present a series of prompts for password criteria
+// Function to prompt user for password options
+
+//     * Length of password
+var passLength = parseInt(prompt("What length of password would you like? (between 8 and 128 characters):"));
+//       * At least 8 characters but no more than 128.
+if (passLength < 8 || passLength > 128) {
+    alert("Try again, enter a number between 8 and 128.");
+  } else if (isNaN(passLength)) {
+    alert("Not a Number. Please enter a number between 8 and 128.");
+  }
+
+//     * Character types
+//       * Lowercase
+//       * Uppercase
+//       * Numeric
+//       * Special characters ($@%&*, etc)
+
+const isLowerCase = confirm("Include lowercase chars?");
+const isUpperCase = confirm("Include uppercase chars?");
+const isNumeric = confirm("Include numeric chars?");
+const isSpecial = confirm("Include special chars?");
+
+//   * Code should validate for each input and at least one character type should be selected
+
+if (!isLowerCase && !isUpperCase && !isNumeric && !isSpecial) {
+    alert("You haven't picked a char type.");
+  }
+//    * make chosen characteristics as var
+
+let chosenCharacteristics = "";
+  if (isLowerCase) {
+    chosenCharacteristics += lowerCasedCharacters;
+  }
+  if (isUpperCase) {
+    chosenCharacteristics += upperCasedCharacters;
+  }
+  if (isNumeric) {
+    chosenCharacteristics += numericCharacters;
+  }
+  if (isSpecial) {
+    chosenCharacteristics += specialCharacters;
+  }
+
+//   * Once prompts are answered then the password should be generated
+// function to generate password
 function generatePassword() {
 
+let password = "";
+  for (let i = 0; i < passLength; i++) {
+    let randomChar = Math.floor(Math.random() * chosenCharacteristics);
+    password += chosenCharacteristics.charAt(randomChar);
+  }
+  return;
 }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
+
+
+//------------------------------------------------------//
+//---------make this happen when button clicked --------//
+//------------------------------------------------------//
+
+
+//   * then displayed in an alert or written to the page
 
 // Write password to the #password input
 function writePassword() {
@@ -114,5 +183,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
